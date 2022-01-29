90abbb03a50d237a77ca916bba52557a1eee6cac
vitormarco <vitoradrianomarco@gmail.com>
2022-01-29 09:52:36 -0300

    Create summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <span>Income</span>
+          <img src={incomeImg} alt="Income" />
+        </header>
+        <strong>R$1000.00</strong>
+      </div>
+      <div>
+        <header>
+          <span>outcome</span>
+          <img src={outcomeImg} alt="outcome" />
+        </header>
+        <strong>R$-500.00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <span>Total</span>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500.00</strong>
+      </div>
+    </Container>
+  )
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     --background: #f0f2f5;
     --red: #E62E4D;
     --blue: #5429CC;
+    --green: #33cc95;
 
     --blue-light: #6933FF;
     
