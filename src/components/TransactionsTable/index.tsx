6efe2acd81d3c71a6b3a0e5977f470956b8413bc
vitormarco <vitoradrianomarco@gmail.com>
2022-01-29 10:11:11 -0300

    Create transactions table

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Value</th>
+            <th>Category</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Website development</td>
+            <td className="deposit">R$12000</td>
+            <td>Development</td>
+            <td>2022/01/01</td>
+          </tr>
+          <tr>
+            <td>Rent</td>
+            <td className="withdraw">-R$1200</td>
+            <td>House</td>
+            <td>2022/01/17</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
